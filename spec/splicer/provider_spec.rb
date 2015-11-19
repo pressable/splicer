c6a0a52896138feb25479c0d6a6ea4bfbca45abb
Joshua Stowers <stowers.joshua@live.com>
2015-11-19 15:01:28 -0600

    Corrected logic in provider_spec All tests now pass

--- spec/splicer/provider_spec.rb
+++ spec/splicer/provider_spec.rb
@@ -7,26 +7,26 @@ describe Splicer::Provider do
 
   describe '#create_zone' do
     subject { provider.create_zone(zone) }
-    it { should be_false }
+    it { should be_falsy }
   end
 
   describe '#delete_zone' do
     subject { provider.delete_zone(zone) }
-    it { should be_false }
+    it { should be_falsy }
   end
 
   describe '#create_record_in_zone' do
     subject { provider.create_record_in_zone(record, zone) }
-    it { should be_false }
+    it { should be_falsy }
   end
 
   describe '#update_record_in_zone' do
     subject { provider.update_record_in_zone(record, zone) }
-    it { should be_false }
+    it { should be_falsy }
   end
 
   describe '#delete_record_in_zone' do
     subject { provider.delete_record_in_zone(record, zone) }
-    it { should be_false }
+    it { should be_falsy }
   end
 end
